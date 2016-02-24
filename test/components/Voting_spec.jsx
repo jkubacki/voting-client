40b40ac667e580d7c2bb9d22a19569d6573a28aa
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-24 21:01:19 +0100

    Rerender component when new immutable dataset is passed

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -6,6 +6,7 @@ import {
   Simulate
 } from 'react-addons-test-utils';
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 import Voting from '../../src/components/Voting';
 
@@ -87,5 +88,25 @@ describe('Voting', () => {
     );
     firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
     expect(firstButton.textContent).to.equal('Trainspotting');
+  });
+
+  it('does update DOM when prop changes', () => {
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const container = document.createElement('div');
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Trainspotting');
+
+    const newPair = pair.set(0, 'Sunshine');
+    component = ReactDOM.render(
+      <Voting pair={newPair} />,
+      container
+    );
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Sunshine');
   });
 });
