6752779c4ed3185b4cfc8598d8ef4b4b71e9b6a3
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-24 19:42:38 +0100

    Invoke vote callback when button is clicked

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -20,5 +20,18 @@ describe('Voting', () => {
     expect(buttons[1].textContent).to.equal('28 Days Later');
   });
 
+  it('invokes callback when button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+        vote={vote}/>
+    );
+
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
   });
 });
