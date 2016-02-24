80885b8bd5c059f191f472de56a742b59e893841
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-24 21:24:52 +0100

    Add basic router configuration

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App'
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} hasVoted="Trainspotting" winner="Trainspotting" />,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
 );
