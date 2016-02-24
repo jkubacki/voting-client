b2aab756d7a7b1290a9592acc0daebeb3e7d12ac
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-24 21:35:47 +0100

    Add Results component with route

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Voting from './components/Voting';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App'
+import Voting from './components/Voting';
+import Results from './components/Results';
 
 const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
 </Route>;
 
