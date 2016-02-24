aa9067c042ad29c645aa06979a999642c7b0ba04
Jakub Kubacki <kubacki.jk@gmail.com>
2016-02-24 20:28:54 +0100

    Remove extracted methods

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -3,15 +3,6 @@ import Winner from './winner'
 import Vote from './vote'
 
 export default React.createClass({
-  getPair: function() {
-    return this.props.pair || [];
-  },
-  isDisabled: function() {
-    return !!this.props.hasVoted;
-  },
-  hasVotedFor: function(entry) {
-    return this.props.hasVoted === entry;
-  },
   render: function() {
     return <div>
       { this.props.winner ?
